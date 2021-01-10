f59759c298492d1d878b62d817a62cb93f9f4012
fepu08 <ferencbesenyei@protonmail.com>
2021-01-10 17:20:56 +0100

    created Profile model

---
+++ hvk-application/models/Profile.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    location: [
+        {
+            address: {
+                type: String
+            },
+            city: {
+                type: String
+            },
+            country: {
+                type: String
+            }
+        }
+    ],
+    skills: {
+        type: [String],
+        required: true
+    },
+    githubusername: {
+        type: String,
+        unique: true,
+        sparse: true
+    },
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    teams: [
+        {
+            team_id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'team'
+            },
+            name: {
+                type: String
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
