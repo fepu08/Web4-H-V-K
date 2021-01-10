9963b379126df0d3e0b59c2d69de676522d6d371
fepu08 <ferencbesenyei@protonmail.com>
2021-01-10 17:39:06 +0100

    add get user by token to auth route

--- hvk-application/routes/api/auth.js
+++ hvk-application/routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Get user by token
 // @access  Public
-router.get('/', (req, res) => {
-    res.send('User route');
+// By adding auth as second parameter as a request method, make that route protected
+router.get('/', auth, async (request, response) => {
+    try{
+        const user = await User.findById(request.user.id).select('-password');
+        response.json(user);
+    } catch(err) {
+        console.error(err.message);
+        response.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
