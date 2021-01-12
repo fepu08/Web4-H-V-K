1cd1cbebb1ce4c1e4a754cc3b4580d6cfeb04a0f
razagul9713 <44830556+razagul9713@users.noreply.github.com>
2021-01-12 13:33:15 +0100

    getterek hozzáadása

--- hvk-application/routes/api/teams.js
+++ hvk-application/routes/api/teams.js
@@ -6,3 +6,27 @@ const User = require('../../models/User');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const { check, validationResult } = require('express-validator');
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const teams = await Team.find();
+        res.json(teams);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+router.get('/:team_id', auth, async (req, res) => {
+    try {
+        const team = await Team.findById(req.params.team_id);
+        if(!team) return res.status(400).json({ msg: 'Team not found'});
+        res.json(team);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+})
