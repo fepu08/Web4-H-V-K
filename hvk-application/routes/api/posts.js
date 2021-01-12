8b1266219ba365498a9cabb0b4483f1bc2f8a5ce
razagul9713 <44830556+razagul9713@users.noreply.github.com>
2021-01-12 13:30:13 +0100

    .put like és .put unlike hozzáadása

--- hvk-application/routes/api/posts.js
+++ hvk-application/routes/api/posts.js
@@ -7,9 +7,6 @@ const Post = require('../../models/Post');
 const User = require('../../models/User');
 const Team = require('../../models/Team');
 
-
-
-
 router.post('/',
     [
         auth,
@@ -37,11 +34,6 @@ router.post('/',
     }
 })
 
-
-
-
-
-
 router.get('/', auth, async (req, res) => {
     try {
        const posts = await Post.find().sort({date: -1});
@@ -51,7 +43,6 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
-
 
 router.get('/:id', auth, async (req, res) => {
     try {
@@ -68,7 +59,6 @@ router.get('/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
-
 
 router.delete('/:id', auth, async (req, res) => {
     try {
@@ -91,3 +81,39 @@ router.delete('/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        // Check if the post has already been liked by this user
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({msg: "Post already liked"});
+        }
+
+        post.likes.unshift({user: req.user.id});
+        await post.save();
+        res.json(post.likes);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        // Check if the post has already been liked by this user
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({msg: "Post has not been liked yet"});
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+        await post.save();
+        res.json(post.likes);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
